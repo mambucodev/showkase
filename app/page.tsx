9abf411d5261af71463d327ff6ac107cf6d95338
Mambuco <gabriele.giambrone@icloud.com>
2025-06-22 23:58:22 +0200

    Initial Setup and a bit of styling

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="container center page">
+      <div className="card big center">
+        <h1>Mambuco</h1>
+        <p>I'm Mambuco, the best developer out there.</p>
+      </div>
+    </div>
+  )
+}
